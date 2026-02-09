e139966d4526f3b9cf2a28a04e8fed84747d88a4
Cathal0317 <hlzh0610@gmail.com>
2026-02-09 22:54:37 +0000

    Question 2

--- Question2/Question2.py
+++ Question2/Question2.py
@@ -3,16 +3,16 @@ from functools import lru_cache
 
 def zero_player_wins(n: int, zero_starts: bool) -> bool:
     @lru_cache(None)
-    def solve(state, turn):  # turn: 0이면 0-player 차례, 1이면 1-player 차례
+    def solve(state, turn):  
         if -1 not in state:
             A = np.array(state).reshape(n, n)
             return int(round(np.linalg.det(A))) == 0
 
         empties = [i for i, v in enumerate(state) if v == -1]
 
-        if turn == 0:  # 0-player: exists winning move
+        if turn == 0:  # 0-player
             return any(solve(state[:k] + (0,) + state[k+1:], 1) for k in empties)
-        else:          # 1-player: for all moves still winning
+        else:          # 1-player
             return all(solve(state[:k] + (1,) + state[k+1:], 0) for k in empties)
 
     start_turn = 0 if zero_starts else 1
